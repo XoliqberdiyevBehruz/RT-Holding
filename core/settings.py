151f2c97a379c781ca2ed7c51fa68e9d34814e4d
behruz <xoliqbediyevbehruz@gmail.com>
2024-11-22 15:02:52 +0500

    project is done

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # packages
+    'corsheaders',
     'drf_yasg',
     'rest_framework',
     'django_filters',
@@ -41,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -204,3 +206,12 @@ CACHES = {
         }
     }
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # For local development
+    "http://127.0.0.1:8000",  # If you're testing with local servers
+]
+
+CSRF_ALLOWED_ALL = True
+
